d7967db3d58de67cd7064865ab66ce10307e40a1
Chris Cox <chriscox@lsu.edu>
2025-10-30 16:58:16 -0500

    fixing some obvious mapping issues... logic of joins seems correct

--- 001-LoadPreviousDB-MapClean-CRC.R
+++ 001-LoadPreviousDB-MapClean-CRC.R
@@ -9,7 +9,7 @@ source("R/dbSelectHelpers.R")
 source("R/read_all_tables.R")
 
 
-# Database list ---
+# Database list ----
 db_files <- c(
   "semantic_association_validation-crc.db",
   "semantic_association_validation-crc_2.db",
@@ -17,11 +17,11 @@ db_files <- c(
 )
 
 
-# Load from database ---
+# Load from database ----
 tbls <- read_all_tables(db_files)
 
 
-# Reconcile CUE indices across databases ---
+# Reconcile CUE indices across databases ----
 all_cues <- list_rbind(tbls$cues, names_to = "study_id") |>
   rename(old_id = id)
 
@@ -46,7 +46,7 @@ id_map_cues <- left_join(
   relocate(new_id, .after = old_id)
 
 
-## Reconcile RESPONSE indices across databases ---
+# Reconcile RESPONSE indices across databases ----
 all_resps <- list_rbind(tbls$responses, names_to = "study_id") |>
   rename(old_id = id)
 
@@ -60,38 +60,70 @@ id_map_resps <- left_join(all_resps, rename(distinct_resps, new_id = id)) |>
   relocate(new_id, .after = old_id)
 
 
-## Revise indices in CUE_RESPONSE ---
+# Revise indices in CUES_RESPONSES ----
 cue_resp_map <- tbls$cues_responses |>
   list_rbind(names_to = "study_id") |>
-  select(study_id, old_id = id, old_cue_id = cue_id, old_resp_id = response_id) |>
-  left_join(
-    select(id_map_cues, study_id, old_cue_id = old_id, cue_id = new_id),
-    by = c("study_id", "old_cue_id")
+  select(
+    study_id,
+    old_id = id,
+    old_cue_id = cue_id,
+    old_resp_id = response_id
   ) |>
   left_join(
-    select(id_map_resps, study_id, old_resp_id = old_id, response_id = new_id),
-    by = c("study_id", "old_resp_id")
+    id_map_cues |> select(
+      study_id,
+      old_cue_id = old_id,
+      cue_id = new_id
+    ),
+    by = join_by(study_id, old_cue_id)
+  ) |>
+  left_join(
+    id_map_resps |> select(
+      study_id,
+      old_resp_id = old_id,
+      response_id = new_id
+    ),
+    by = join_by(study_id, old_resp_id)
   ) |>
   mutate(new_id = seq_len(n())) |>
   relocate(new_id, study_id, cue_id, response_id)
 
+
+## Sanity check: cues_responses revision ----
+# Map new and old response IDs to corresponding strings and check for equality
 tmp <- cue_resp_map |>
   group_by(study_id) |>
   group_split() |>
-  map2(tbls$cues, ~ left_join(.x, rename(.y, old_cue_id=id, old_cue=cue), by = join_by(old_cue_id))) |>
-  map2(tbls$responses, ~ left_join(.x, rename(.y, old_resp_id=id, old_resp=response), by = join_by(old_resp_id))) |>
+  map2(tbls$cues, function(resp_map, cues) {
+    left_join(
+      resp_map,
+      cues |> rename(old_cue_id=id, old_cue=cue),
+      by = join_by(old_cue_id)
+    )
+  }) |>
+  map2(tbls$responses, function(resp_map, responses) {
+    left_join(
+      resp_map,
+      responses |> rename(old_resp_id=id, old_resp=response),
+      by = join_by(old_resp_id)
+    )
+  }) |>
   list_rbind() |>
-  left_join(rename(distinct_resps, response_id = id), by = join_by(response_id))
+  left_join(
+    distinct_resps |> rename(response_id=id),
+    by = join_by(response_id)
+  )
 
 tmp |> mutate(x = old_resp == response) |>  pull(x) |> all() 
 
+map(tbls$responses, ~.x |> filter(response == "toyota"))
 
 ## Revise indices in RESPONSE_MAP ---
 resp_map <- tbls$response_map |>
   list_rbind(names_to = "study_id") |>
   select(
     study_id,
-    old_id = id,
+    old_id = cue_response_id,
     kuperman_id,
     subtlex_id,
     revision,
@@ -99,28 +131,42 @@ resp_map <- tbls$response_map |>
     timestamp
   ) |>
   left_join(
-    select(cue_resp_map, study_id, old_id, cue_id, response_id),
+    cue_resp_map |> select(study_id, old_id, cue_id, response_id),
     by = join_by(study_id, old_id)
   ) |>
   mutate(new_id = seq_len(n())) |>
   relocate(new_id, old_id, study_id, cue_id, response_id)
-  
 
-# Sanity Check ---
-tmp <- map2(tbls$response_map, tbls$cues_responses, ~ {
+inconsistent_mapping <- resp_map |>
+  arrange(response_id) |>
+  group_by(response_id) |>
+  filter(if_any(c(revision, kuperman_id, subtlex_id), ~ n_distinct(.x) > 1))
+
+# Load Stan's revisions ----
+stan_rev <- readr::read_csv("data/cross_study_cleanedRevisions.csv")
+inconsistent_mapping |>
+  left_join(distinct_cues |> rename(cue_id=id)) |>
+  left_join(distinct_resps |> rename(response_id=id)) |>
   left_join(
-    select(.x, -cue_id),
-    rename(.y, cue_response_id = id),
-    by = "cue_response_id"
-  )
-}) |>
-    map2(tbls$cues, ~ left_join(.x, rename(.y, cue_id = id), by = "cue_id")) |>
-    map2(tbls$responses, ~ left_join(.x, rename(.y, response_id = id), by = "response_id")) |>
-    map(~ {
-      relocate(.x, cue_id, response_id, cue, response, kuperman_id, subtlex_id) |>
-        arrange(response)
-    })
+    stan_rev |>
+      select(
+        response,
+        cue,
+        stan_revision = revision,
+        stan_subtlex_id = subtlex_id,
+        stan_kuperman_id = kuperman_id
+      ) |>
+      distinct()
+    ) |>
+  select(study_id:revision, cue:stan_kuperman_id) |>
+  filter(cue == "daddy")
 
+cue_resp_words <- cue_resp_map |>
+  left_join(distinct_cues |> rename(cue_id=id)) |>
+  left_join(distinct_resps |> rename(response_id=id))
+
+cue_resp_words |>
+  filter(response == "toyota")
 
 ### Clean dupes
 cleaned <- read.csv("clean_dups.csv") %>%
